e9e79e72524d450230c853462e5e31c02fefa99b
kingno3l <nuelluiz@gmail.com>
2024-08-25 17:46:20 +0100

    seeded data

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use DB;
+use Illuminate\Support\Facades\Hash;
+
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            //Admin
+            [
+                'name' => 'John Doe',
+                'email' => 'admin@example.com',
+                'password' => Hash::make('111'),
+                'role' => 'admin',
+                'status' => '1',
+            ],
+
+            //User
+            [
+                'name' => 'John Doe',
+                'email' => 'user@example.com',
+                'password' => Hash::make('111'),
+                'role' => 'user',
+                'status' => '1',
+            ],
+        ]);
+    }
+}

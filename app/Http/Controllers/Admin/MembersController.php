38025094042e93103e62819cd1884a99fd5dff71
kingno3l <nuelluiz@gmail.com>
2024-09-21 12:08:34 +0100

    implemented academic qualification

--- app/Http/Controllers/Admin/MembersController.php
+++ app/Http/Controllers/Admin/MembersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\ProfileManagement\Document;
 
 
 class MembersController extends Controller
@@ -37,24 +38,44 @@ class MembersController extends Controller
         return view('admin.member.profile', compact('profileData', 'socials'));
     }
 
+    // public function showAcademicQualifications($id)
+    // {
+    //     $profileData = User::findOrFail($id);
+    //     $socials = $profileData->socials()->first(); // Retrieve the first social record
+
+    //     $academicQualifications = $profileData->academicQualifications;
+    //     return view('admin.member.academic_qualifications', compact('profileData', 'academicQualifications', 'socials'));
+    // }
+
     public function showAcademicQualifications($id)
     {
-        $member = User::findOrFail($id);
-        $academicQualifications = $member->academicQualifications; 
-        return view('admin.member.academic_qualifications', compact('member', 'academicQualifications'));
+        $profileData = User::findOrFail($id);
+        $socials = $profileData->socials()->first(); // Retrieve the first social record
+
+        $academicQualifications = $profileData->academicQualifications;
+
+        // Fetch uploaded files related to academic qualifications
+        $uploadedFiles = Document::where('user_id', $id)
+            ->where('documentable_type', 'academic qualification')
+            ->get();
+
+        return view('admin.member.academic_qualifications', compact('profileData', 'academicQualifications', 'socials', 'uploadedFiles'));
     }
+
+
+
 
     public function showEmploymentHistory($id)
     {
-        $member = User::findOrFail($id);
-        $employmentHistory = $member->employmentHistory;
-        return view('admin.member.employment_history', compact('member', 'employmentHistory'));
+        $profileData = User::findOrFail($id);
+        $employmentHistory = $profileData->employmentHistory;
+        return view('admin.member.employment_history', compact('profileData', 'employmentHistory'));
     }
 
     public function showNextOfKin($id)
     {
-        $member = User::findOrFail($id);
-        $nextOfKin = $member->nextOfKin; 
-        return view('admin.member.next_of_kin_referee', compact('member', 'nextOfKin'));
+        $profileData = User::findOrFail($id);
+        $nextOfKin = $profileData->nextOfKin; 
+        return view('admin.member.next_of_kin_referee', compact('profileData', 'nextOfKin'));
     }
 }
